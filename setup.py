b7ba4a4062ef5ef83c4a201206d572fd270c33f6
yeelaine29 <yeelainehou@gmail.com>
2023-05-19 12:58:09 +0800

    Update Require Packages

--- setup.py
+++ setup.py
@@ -8,4 +8,9 @@ setup(
     author_email="yeelainehou@gmail.com",
     url="https://github.com/hhe1ibeb/streetviewext",
     packages=["streetviewext"],
+    install_requires=[
+        'numpy',
+        'pandas==1.2.3',
+        'matplotlib==3.4.0'
+    ],
 )
